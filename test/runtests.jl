a7b08c79334e6bb4b97ba37c005b8fa98f20f8e4
Jose Esparza <pebeto99@gmail.com>
2026-04-17 16:14:29 -0500

    Removing old test cases, and including new ones

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,6 +4,23 @@ using DataFrames
 using Statistics
 using Test
 
-include("core.jl")
+@testset "Data" verbose = true begin
+    include("data/pool.jl")
+    include("data/encoding.jl")
+end
+
+@testset "Training" verbose = true begin
+    include("training/regression.jl")
+    include("training/classification.jl")
+    include("training/categorical.jl")
+    include("training/training_options.jl")
+end
+
+@testset "API" verbose = true begin
+    include("api/prediction.jl")
+    include("api/feature_importance.jl")
+    include("api/io.jl")
+    include("api/cv.jl")
+end
 
 Aqua.test_all(MichiBoost)
